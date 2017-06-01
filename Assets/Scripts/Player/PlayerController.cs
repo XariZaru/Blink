65582bfaa5c868a9211e2a6d7c707693a1f5343f
XariZaru <jonsnguyening@gmail.com>
2017-06-01 16:23:52 -0700

    - Initial Setup

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+
+    float speed = .2f;
+
+	// Use this for initialization
+	void Start () {        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if (!hasAuthority)
+        //return;
+
+        var pos = Camera.main.WorldToScreenPoint(transform.position);
+        var dir = Input.mousePosition - pos;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        transform.position += movement * speed;
+    }
+
+    void FixedUpdate()
+    {
+        
+
+    }
+    
+}
---
+++ Assets/Scripts/Player/Server.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        NetworkServer.Listen("25.1.50.65", 8484);        
+        NetworkServer.SpawnObjects();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
