278207c14f281e17d0714d4754fc132ac59d19a2
XariZaru <jonsnguyening@gmail.com>
2017-06-01 18:02:59 -0700

    Allow Player GameObject to shoot projectiles

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,17 +5,22 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
 
-
+    public GameObject bulletPrefab;
     float speed = .2f;
+    float bulletSpeed = 100.0f;
+    float fireRate = 0.5f;
+    float lastShot = 0.0f;
+   
 
 	// Use this for initialization
 	void Start () {        
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //if (!hasAuthority)
-        //return;
+        if (!hasAuthority)
+            return;
 
         var pos = Camera.main.WorldToScreenPoint(transform.position);
         var dir = Input.mousePosition - pos;
@@ -27,6 +32,23 @@ public class PlayerController : NetworkBehaviour {
 
         Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
         transform.position += movement * speed;
+
+
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time > fireRate + lastShot)
+            {
+                Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+                Vector2 curPos = new Vector2(transform.position.x, transform.position.y);
+                Vector2 direction = target - curPos;
+                direction.Normalize();
+                Quaternion rotatio = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90);
+                GameObject projectile = (GameObject)Instantiate(bulletPrefab, curPos, Quaternion.identity);
+                projectile.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+                lastShot = Time.time;
+            }
+            
+        }
     }
 
     void FixedUpdate()
@@ -36,3 +58,4 @@ public class PlayerController : NetworkBehaviour {
     }
     
 }
+
--- Assets/Scripts/Player/Server.cs
+++ Assets/Scripts/Player/Server.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class Server : MonoBehaviour {
+public class Server : NetworkManager {
+    
+    public GameObject player;
+
 
 	// Use this for initialization
-	void Start () {
-        NetworkServer.Listen("25.1.50.65", 8484);        
-        NetworkServer.SpawnObjects();
+	void Start () {        
+        
+        NetworkClient client = new NetworkClient();
+        client.Connect("25.1.50.65", 8484);
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void RegisterPrefabs()
+    {
+        ClientScene.RegisterPrefab(player);
+    }
 }
