38425e848493ea147c88b3449acdc4f43341a4e8
Xari <jonsnguyening@gmail.com>
2017-06-16 13:33:48 -0700

    - Loading file

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Loading : MonoBehaviour {
+
+	GameObject bullet, teleportBullet;
+
+	// Use this for initialization
+	void Start () {
+		ClientScene.RegisterPrefab (bullet);
+		ClientScene.RegisterPrefab (teleportBullet);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
